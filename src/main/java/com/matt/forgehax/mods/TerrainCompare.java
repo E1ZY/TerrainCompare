e4bde765d88f5f613a1d9c0247532df73f35f9ab
E1ZY <petya.potapenko@gmail.com>
2020-08-23 12:05:24 +0300

    added: managed chunks to render more than 1 chunk colors difference types

--- src/main/java/com/matt/forgehax/mods/TerrainCompare.java
+++ src/main/java/com/matt/forgehax/mods/TerrainCompare.java
@@ -14,10 +14,25 @@ import com.mojang.realmsclient.util.Pair;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.chunk.RenderChunk;
+import net.minecraft.client.renderer.entity.RenderFallingBlock;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexBuffer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.client.event.DrawBlockHighlightEvent;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.jline.utils.DiffHelper;
 import org.lwjgl.opengl.GL11;
@@ -32,7 +47,7 @@ public class TerrainCompare extends ToggleMod {
   private final TerrainCompareUtils utils;
   private final ArrayList<ChunkPos> managedChunks; // chunks that are in render distance
   private final ArrayList<Pair<BlockPos, Difference>> difArray;
-  private int renderDistance;
+  private final int renderDistance;
   private int playerX;
   private int playerZ;
 
@@ -41,7 +56,7 @@ public class TerrainCompare extends ToggleMod {
     utils = new TerrainCompareUtils();
     managedChunks = new ArrayList<>();
     difArray = new ArrayList<>();
-    renderDistance = 3; // default is 3
+    renderDistance = 0; // default is 3
   }
 
   @SubscribeEvent
@@ -78,6 +93,50 @@ public class TerrainCompare extends ToggleMod {
       }
     }
 
+  }
+
+  @SubscribeEvent
+  public void highlightGhostBlock(RenderWorldLastEvent event) {
+    IBlockState stateToRender = Blocks.DIAMOND_BLOCK.getDefaultState();
+    Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+    Entity entity = Minecraft.getMinecraft().getRenderViewEntity();
+
+    GlStateManager.pushMatrix();
+    GlStateManager.disableLighting();
+    GlStateManager.enableAlpha();
+    GlStateManager.disableDepth();
+    GlStateManager.enableBlend();
+    GlStateManager.color(1,1,1,0.25F);
+    // GlStateManager.alphaFunc(0, 0);
+    Tessellator tessellator = Tessellator.getInstance();
+    BufferBuilder vertexbuffer = tessellator.getBuffer();
+
+    double d0 = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * (double)event.getPartialTicks();
+    double d1 = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * (double)event.getPartialTicks();
+    double d2 = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * (double)event.getPartialTicks();
+
+    vertexbuffer.begin(7, DefaultVertexFormats.BLOCK);
+    // BlockPos blockpos = new BlockPos(position.getX(), position.getY(), position.getZ());
+    Tessellator.getInstance().getBuffer().setTranslation(-d0, -d1, -d2);
+    // GlStateManager.translate(position.getX(), position.getY(), position.getZ());
+
+    BlockRendererDispatcher blockrendererdispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
+    for (Pair<BlockPos, Difference> pair : difArray) {
+      // Tessellator.getInstance().getBuffer().setTranslation(-pair.first().getX() + 0.5, -pair.first().getY(), -pair.first().getZ() + 0.5);
+      blockrendererdispatcher.getBlockModelRenderer().renderModel(
+          Minecraft.getMinecraft().world,
+          blockrendererdispatcher.getModelForState(utils.getBlockState(pair.first())),
+          utils.getBlockState(pair.first()),
+          pair.first(),
+          vertexbuffer,
+          false,
+          MathHelper.getPositionRandom(pair.first()));
+    }
+    tessellator.draw();
+    Tessellator.getInstance().getBuffer().setTranslation(0, 0, 0);
+    GlStateManager.disableBlend();
+    GlStateManager.enableLighting();
+    GlStateManager.popMatrix();
   }
 
   @SubscribeEvent
